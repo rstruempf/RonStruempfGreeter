526008a55e1ea5a46fcaacf34fd56ccecbef9ec9
Ron Struempf <rstruem1@my.westga.edu>
2016-03-01 22:54:54 -0500

    Tutorial Part 3 Complete, tests pass

--- app/src/androidTest/java/com/ronstruempf/ronstruempfgreeter/MainActivityTests.java
+++ app/src/androidTest/java/com/ronstruempf/ronstruempfgreeter/MainActivityTests.java
@@ -1,6 +1,10 @@
 package com.ronstruempf.ronstruempfgreeter;
 
 import android.test.ActivityInstrumentationTestCase2;
+import android.test.TouchUtils;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.TextView;
 
 /**
  * Test MainActivity
@@ -12,9 +16,38 @@ public class MainActivityTests extends ActivityInstrumentationTestCase2<MainActi
         super(MainActivity.class);
     }
 
-    public void testActivityExists() throws Exception {
+    public void testActivityExists() {
         MainActivity activity = getActivity();
         assertNotNull(activity);
     }
+
+    public void testGreet() {
+        MainActivity activity = getActivity();
+        //
+        // set the value of the name edit field
+        //
+        final EditText nameEditText = (EditText)activity.findViewById(R.id.greet_edit_text);
+        getInstrumentation().runOnMainSync(new Runnable() {
+            @Override
+            public void run() {
+                nameEditText.requestFocus();
+            }
+        });
+        getInstrumentation().waitForIdleSync();
+        getInstrumentation().sendStringSync("Jake");
+        //
+        // Tap the Greet button
+        //
+        Button greetButton = (Button)activity.findViewById(R.id.greet_button);
+        TouchUtils.clickView(this, greetButton);
+        //
+        // The display field should now display a greeting to Jake
+        //
+        TextView greetMessage =
+                (TextView) activity.findViewById(R.id.message_text_view);
+        String actualText = greetMessage.getText().toString();
+        assertEquals("Hello, Jake!", actualText);
+    }
+
 
 }
--- app/src/main/java/com/ronstruempf/ronstruempfgreeter/MainActivity.java
+++ app/src/main/java/com/ronstruempf/ronstruempfgreeter/MainActivity.java
@@ -8,6 +8,8 @@ import android.support.v7.widget.Toolbar;
 import android.view.View;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.widget.EditText;
+import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -26,6 +28,8 @@ public class MainActivity extends AppCompatActivity {
                         .setAction("Action", null).show();
             }
         });
+
+
     }
 
     @Override
@@ -48,5 +52,13 @@ public class MainActivity extends AppCompatActivity {
         }
 
         return super.onOptionsItemSelected(item);
+    }
+
+    public void greenButtonClicked(View view) {
+        EditText greetEditText = (EditText)findViewById(R.id.greet_edit_text);
+        String name = greetEditText.getText().toString();
+        String greeting = String.format("Hello, %s!", name);
+        TextView messageTextView = (TextView)findViewById(R.id.message_text_view);
+        messageTextView.setText(greeting);
     }
 }
